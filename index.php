f06279f94ea8f12381ecfdbd98ea4f4149aa6642
Izaias Cavalcanti <izaias@iuqo.com>
2016-05-19 00:51:41 -0300

    update

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>temporary</title>
+</head>
+<body>
+	<a href="https://heroku-test-izaias.herokuapp.com/website">goto website</a>
+</body>
+</html>
--- website/index.php
+++ website/index.php
@@ -4,6 +4,6 @@
 	<title>test</title>
 </head>
 <body>
-	<h1>test 3</h1>
+	<h1>test 4</h1>
 </body>
 </html>
